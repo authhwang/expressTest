e0524d666147a6a044895e18329ea123e3b4e927
郭漫丽 <404544332@qq.com>
2017-05-30 10:52:04 +0800

    add fortune module

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var handlebars = require('express-handlebars')
             .create({defaultLayout : 'main'});
+var randomFortune = require('./lib/fortune.js').getRandomFortunes;
 
 var app = express();
 app.engine('handlebars',handlebars.engine);
@@ -8,14 +9,6 @@ app.set('view engine','handlebars');
 
 app.set('port',process.env.PORT || 3000);
 app.use(express.static(__dirname + '/public'));
-
-var fortunes = [
-  "Conquer your fears or they will conquer you.", 
-  "Rivers need springs.",
-  "Do not fear what you don't know.",
-  "You will have a pleasant surprise.", "Whenever possible, keep it simple.",
-];
-
 
 
 app.get('/',function(req,res){
@@ -27,9 +20,8 @@ app.get('/',function(req,res){
 app.get('/about',function(req,res){
     // res.type('text/plain');
     // res.send('about Meadowlark travel');
-    var randomFortune =
-          fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about',{fortune : randomFortune});
+    
+    res.render('about',{fortune : randomFortune()});
     
 });
 
